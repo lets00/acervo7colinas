f5b4ae3e7824b9c090637185541bdac9dfd980b5
Luana Barbosa <luanahbbarbosa2005@gmail.com>
2026-03-07 22:52:01 -0300

    feat: implementação do componente de cadastro de livros

---
+++ acervo-7-colinas/src/Componentes/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+  AppBar, Toolbar, Typography, Button, InputBase, 
+  Box, Stack, IconButton 
+} from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+
+/*
+function Header(){
+    return(
+
+    )
+}
+
+export default Header;
+*/
