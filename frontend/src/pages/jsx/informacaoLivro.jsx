16ebca9b9a461b406d5bbe9bf21ca0107f51b45b
Jamille Galdino <jamille.galdinogomes@gmail.com>
2026-03-21 14:29:28 -0300

    feat: melhorias no layout da página do livro e tipografia global

--- frontend/src/pages/jsx/informacaoLivro.jsx
+++ frontend/src/pages/jsx/informacaoLivro.jsx
@@ -4,6 +4,7 @@ import "../css/informacaoLivros.css";
 import SectionHeader from "../../components/SectionHeader";
 import BookCard from "../../components/BookCard";
 import Comentario from "../../components/comentario";
+import Header from "../../components/Header";
 
 import iconInfo from "../../assets/info.png";
 import iconCalendario from "../../assets/calendario.png";
@@ -17,6 +18,8 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+
+import Box from "@mui/material/Box";
 
 import IconButton from "@mui/material/IconButton";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
@@ -65,145 +68,157 @@ function InformacaoLivro() {
     if (!livro) return <p>Carregando...</p>;
 
     return (
-        <div className="page-informacao-livro">
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            minHeight: "100vh",
+            bgcolor: "#fff",
+            marginTop: "-55px"
+        }}>
+        
+            <Header />
+            <Box sx={{ width: "100%", maxWidth: "1250px", px:2 , marginLeft: "60px", marginRight: "60px"}}>
 
-            {/* 🔥 TOPO DO LIVRO */}
-            <div className="livro-info-topo">
+                {/* 🔥 TOPO DO LIVRO */}
+                <div className="livro-info-topo">
 
-                <div className="livro-capa-container">
-                    <img
-                        src={`http://localhost:3000${livro.img}`}
-                        alt={livro.titulo}
-                        className="livro-capa"
-                    />
+                    <div className="livro-capa-container">
+                        <img
+                            src={`http://localhost:3000${livro.img}`}
+                            alt={livro.titulo}
+                            className="livro-capa"
+                        />
 
-                    <Button
-                        variant="contained"
-                        className="btn-prateleira"
-                    >
-                        ADICIONAR À PRATELEIRA
-                    </Button>
+                        <Button
+                            variant="contained"
+                            className="btn-prateleira"
+                        >
+                            ADICIONAR À PRATELEIRA
+                        </Button>
+                    </div>
+
+                    <div className="livro-detalhes">
+                        <h1>{livro.titulo}</h1>
+                        <h3>{livro.autor}</h3>
+
+                        <div className="livro-avaliacao">
+                            <Rating value={livro.avaliacao} precision={0.5} readOnly />
+                            <span>{livro.avaliacao}</span>
+                        </div>
+                        <div className="livro-meta">
+
+                            <div className="meta-item">
+                                <img src={iconLivroFechado} alt="editora" />
+                                <span>{livro.editora}</span>
+                            </div>
+
+                            <div className="meta-item">
+                                <img src={iconLivroAberto} alt="paginas" />
+                                <span>{livro.paginas} páginas</span>
+                            </div>
+
+                            <div className="meta-item">
+                                <img src={iconCalendario} alt="ano" />
+                                <span>{livro.ano}</span>
+                            </div>
+
+                            <div className="meta-item">
+                                <img src={iconIsbn} alt="isbn" />
+                                <span>ISBN: {livro.isbn}</span>
+                            </div>
+
+                            <div className="meta-item">
+                                <img src={iconInfo} alt="idioma" />
+                                <span>{livro.idioma}</span>
+                            </div>
+
+                        </div>
+
+                        <div className="livro-generos">
+                            <p>Gêneros:</p>
+                            {livro.generos.map((g, index) => (
+                                <span key={index}>{g}</span>
+                            ))}
+                        </div>
+
+                        <p className="livro-descricao">{livro.descricao}</p>
+                    </div>
+
                 </div>
 
-                <div className="livro-detalhes">
-                    <h1>{livro.titulo}</h1>
-                    <h3>{livro.autor}</h3>
+                {/* 📚 EXEMPLARES */}
+                <SectionHeader title="Exemplares" />
 
-                    <div className="livro-avaliacao">
-                        <Rating value={livro.avaliacao} precision={0.5} readOnly />
-                        <span>{livro.avaliacao}</span>
-                    </div>
-                    <div className="livro-meta">
+                <div className="exemplares-section">
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>ID</TableCell>
+                                    <TableCell>SEÇÃO</TableCell>
+                                    <TableCell>DISPONIBILIDADE</TableCell>
+                                </TableRow>
+                            </TableHead>
 
-                        <div className="meta-item">
-                            <img src={iconLivroFechado} alt="editora" />
-                            <span>{livro.editora}</span>
-                        </div>
+                            <TableBody>
+                                {exemplares.map((exemplar) => (
+                                    <TableRow key={exemplar.id}>
+                                        <TableCell>{exemplar.id}</TableCell>
+                                        <TableCell>{exemplar.secao}</TableCell>
+                                        <TableCell>
+                                            {exemplar.disponivel ? "Disponível" : "Indisponível"}
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
 
-                        <div className="meta-item">
-                            <img src={iconLivroAberto} alt="paginas" />
-                            <span>{livro.paginas} páginas</span>
-                        </div>
+                {/* 📖 RELACIONADOS */}
+                <SectionHeader title="Os leitores também gostaram" />
 
-                        <div className="meta-item">
-                            <img src={iconCalendario} alt="ano" />
-                            <span>{livro.ano}</span>
-                        </div>
+                <div className="carousel-container">
 
-                        <div className="meta-item">
-                            <img src={iconIsbn} alt="isbn" />
-                            <span>ISBN: {livro.isbn}</span>
-                        </div>
+                    <IconButton onClick={() => scrollLeft(destaquesRef)}>
+                        <ChevronLeftIcon />
+                    </IconButton>
 
-                        <div className="meta-item">
-                            <img src={iconInfo} alt="idioma" />
-                            <span>{livro.idioma}</span>
-                        </div>
-
-                    </div>
-
-                    <div className="livro-generos">
-                        {livro.generos.map((g, index) => (
-                            <span key={index}>{g}</span>
+                    <div className="books-container" ref={destaquesRef}>
+                        {relacionados.map((book) => (
+                            <BookCard
+                                key={book.id}
+                                id={book.id}
+                                imagem={`http://localhost:3000${book.img}`}
+                                titulo={book.titulo}
+                                avaliacao={book.avaliacao}
+                            />
                         ))}
                     </div>
 
-                    <p className="livro-descricao">{livro.descricao}</p>
+                    <IconButton onClick={() => scrollRight(destaquesRef)}>
+                        <ChevronRightIcon />
+                    </IconButton>
+
                 </div>
 
-            </div>
+                <SectionHeader title="Avaliações e comentários" />
 
-            {/* 📚 EXEMPLARES */}
-            <SectionHeader title="Exemplares" />
+                    <div className="avaliacoes">
+                        {avaliacoes.map((av) => (
+                            <Comentario
+                                key={av.id}
+                                usuario={av.usuario}
+                                comentario={av.comentario}
+                                nota={av.avaliacao}
+                                tempo={av.tempo}
+                            />
+                        ))}
+                    </div>
 
-            <div className="exemplares-section">
-                <TableContainer>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>ID</TableCell>
-                                <TableCell>SEÇÃO</TableCell>
-                                <TableCell>DISPONIBILIDADE</TableCell>
-                            </TableRow>
-                        </TableHead>
-
-                        <TableBody>
-                            {exemplares.map((exemplar) => (
-                                <TableRow key={exemplar.id}>
-                                    <TableCell>{exemplar.id}</TableCell>
-                                    <TableCell>{exemplar.secao}</TableCell>
-                                    <TableCell>
-                                        {exemplar.disponivel ? "Disponível" : "Indisponível"}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
-
-            {/* 📖 RELACIONADOS */}
-            <SectionHeader title="Os leitores também gostaram" />
-
-            <div className="carousel-container">
-
-                <IconButton onClick={() => scrollLeft(destaquesRef)}>
-                    <ChevronLeftIcon />
-                </IconButton>
-
-                <div className="books-container" ref={destaquesRef}>
-                    {relacionados.map((book) => (
-                        <BookCard
-                            key={book.id}
-                            id={book.id}
-                            imagem={`http://localhost:3000${book.img}`}
-                            titulo={book.titulo}
-                            avaliacao={book.avaliacao}
-                        />
-                    ))}
-                </div>
-
-                <IconButton onClick={() => scrollRight(destaquesRef)}>
-                    <ChevronRightIcon />
-                </IconButton>
-
-            </div>
-
-            <SectionHeader title="Avaliações e comentários" />
-
-                <div className="avaliacoes">
-                    {avaliacoes.map((av) => (
-                        <Comentario
-                            key={av.id}
-                            usuario={av.usuario}
-                            comentario={av.comentario}
-                            nota={av.avaliacao}
-                            tempo={av.tempo}
-                        />
-                    ))}
-                </div>
-
-            </div>
+            </Box>
+        </Box>
     );
 }
 
