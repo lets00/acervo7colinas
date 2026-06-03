2f6039d54077cb2794e1a8d32331ef6b9459f432
DboraGomes <dbora.gomes20@gmail.com>
2026-06-03 11:17:28 -0300

    fix: ajuste controller entregadores

--- backend/src/controllers/entregadorController.js
+++ backend/src/controllers/entregadorController.js
@@ -5,7 +5,7 @@ import { entregadorSchema } from '../validators/entregadorValidator.js';
 export async function criarEntregador(req, res) {
 
     const fotoPerfil = req.files?.fotoPerfil?.[0]?`/cnh/${req.files.fotoPerfil[0].filename}`:null;
-    const fotoPerfil = req.files?.fotoCnh?.[0]?`/cnh/${req.files.fotoCnh[0].filename}`:null;
+    const fotoCnh = req.files?.fotoCnh?.[0]?`/cnh/${req.files.fotoCnh[0].filename}`:null;
 
     const dadosParaValidar = {
         ...req.body,
