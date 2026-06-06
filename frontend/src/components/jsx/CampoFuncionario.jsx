37f07591ee5723b66b9497ab91301792185fa139
luahbarbosa <luanahbbarbosa2005@gmail.com>
2026-06-06 16:47:06 -0300

    fix: ajuste no campo de entregador

--- frontend/src/components/jsx/CampoFuncionario.jsx
+++ frontend/src/components/jsx/CampoFuncionario.jsx
@@ -46,13 +46,8 @@ function CampoFuncionario({ onFileChange = () => {}, errors = {}, resetKey }) {
       </Grid> 
       <Grid item xs={12} md={6}>
         <Grid container spacing={5} justifyContent="center">
-          <Box className="barra" sx={{mt:8}}>
-            <Typography className="barra-texto" sx={{ ml: 65 }}>
-                Foto de Perfil
-            </Typography>
-        </Box>
 
-          <Box className="card" sx={{ mt: -7 }}>
+          <Box className="card" sx={{ mt: 2 }}>
               <Typography className="textoCard textoPerfil" sx={{ ml: 85 }}>
                   Selecionar Foto
               </Typography>
