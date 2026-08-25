26c87d8fba1b05c8dad31a73b11e71a633fece2c
luahbarbosa <luanahbbarbosa2005@gmail.com>
2026-08-25 11:12:45 -0300

    chore: Atualização de Algumas branch para essa

--- frontend/src/pages/jsx/PerfilUsuario.jsx
+++ frontend/src/pages/jsx/PerfilUsuario.jsx
@@ -4,7 +4,7 @@ import "../css/Perfil.css";
 import Footer from "../../components/jsx/Footer.jsx";
 import Header from "../../components/jsx/Header.jsx";
 
-// Seus dados mockados organizados por tipo
+// DADOS MOKADOS PARA TESTE, CASO NÃO TENHA NADA NO LOCALSTORAGE
 const MOCK_USUARIOS = {
   admin: {
     nome: "Fernanda Oliveira",
